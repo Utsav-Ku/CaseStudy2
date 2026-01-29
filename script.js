8c4465458037b3b65dfe653e4553bbd11762395d
AS-MAC-1351 <as-mac-1351@AS-MAC-1351s-MacBook-Air.local>
2026-01-29 23:57:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,150 @@
+let products = [];
+async function fetchProduct(){
+    try{
+        let url = "https://dummyjson.com/products"
+        let response = await fetch(url)
+        const data = await response.json();
+        products = data.products;
+        createCards();
+    } 
+    catch(err){
+        console.log(err);
+    }
+}
+
+function createCards(){
+    const divObj = document.getElementById("product-container");
+    for(let product of products){
+        let title = product.title
+        let description = product.description
+        let price = product.price
+        let category = product.category
+        let rating = product.rating
+        let imageurl = product.images[0]
+
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+        <img src="${imageurl} alt=${title}">
+        <h3>${title}</h3>
+        <p>${description}</p>
+        <p>Cateogry: ${category}</p>
+        <p>Price: ${price}</p>
+        <p>Rating: ${rating}</p>
+        <button class="btn" onclick="addToCart(${product.id})">Add to Cart</button>
+        `;
+
+        divObj.appendChild(card);
+
+        //console.log(title, description, price, category, rating, imageurl);
+    }
+}
+fetchProduct();
+
+function addToCart(productId){
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let product = products.find(p => p.id === productId);
+    cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert("Item added to cart")
+}
+
+function getFromCart(){
+    let products = JSON.parse(localStorage.getItem("cart")) || [];
+    const divObj = document.getElementById("card-contianer");
+
+    for(let product of products){
+        let title = product.title
+        let description = product.description
+        let price = product.price
+        let category = product.category
+        let rating = product.rating
+        let imageurl = product.images[0]
+
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+        <img src="${imageurl} alt=${title}">
+        <h3>${title}</h3>
+        <p>${description}</p>
+        <p>Cateogry: ${category}</p>
+        <p>Price: ${price}</p>
+        <p>Rating: ${rating}</p>
+        <button class="btn">Add to Cart</button>
+        `;
+
+        divObj.appendChild(card);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    getFromCart();
+})
+
+function searchProduct(){
+    let input = document.getElementById("search-input").value.toLowerCase();
+    let matchedData = products.filter(product => product.title.toLowerCase().includes(input));
+    displaySearchProducts(matchedData);
+}
+
+function displaySearchProducts(products){
+    const divObj = document.getElementById("product-container");
+    if(products.length === 0){
+        divObj.innerHTML = "<h3>No Product Found</h3>"
+    }
+
+    for(let product of products){
+        let title = product.title
+        let description = product.description
+        let price = product.price
+        let category = product.category
+        let rating = product.rating
+        let imageurl = product.images[0]
+
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+        <img src="${imageurl} alt=${title}">
+        <h3>${title}</h3>
+        <p>${description}</p>
+        <p>Cateogry: ${category}</p>
+        <p>Price: ${price}</p>
+        <p>Rating: ${rating}</p>
+        <button class="btn" onclick="addToCart(${product.id})">Add to Cart</button>
+        `;
+
+        divObj.appendChild(card);
+
+        //console.log(title, description, price, category, rating, imageurl);
+    }
+}
+
+function debounce(fn, delay){
+    let timer;
+    return function(...args){
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn.apply(this, args);
+        }, delay);
+    }
+}
+
+const handleSearch = debounce(searchProduct, 1000);
+
+function throttle(fn, limit) {
+    let flag = true;
+    return function () {
+        if (flag) {
+            fn();
+            flag = false;
+            setTimeout(() => {
+                flag = true;
+            }, limit);
+        }
+    };
+}
+
+window.addEventListener("scroll", throttle(fn, 2000));
